90224f9ffb3f600c4487ac6ec318835198a9563c
Daniel.Stott <daniel.stott@brighthr.com>
2023-03-16 14:02:11 +0000

    MongoTests use new HttpClient extensions

--- Test.Mongo/MongoTests.cs
+++ Test.Mongo/MongoTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
 using Core;
 using MongoWebApi;
 
@@ -7,10 +7,24 @@ namespace Test.Mongo;
 public class MongoTests : BaseTest
 {
     [Test]
+    public async Task get_dynamic_weather_forecasts()
+    {
+        var (response, content) = await Api.Get("/WeatherForecast");
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(content, Has.Count.EqualTo(3));
+        });
+    }
+
+    [Test]
     public async Task get_weather_forecasts()
     {
-        var responseMessage = await Api.GetAsync("/WeatherForecast");
-        var weatherForecasts = await responseMessage.Content.ReadFromJsonAsync<IEnumerable<WeatherForecast>>();
-        Assert.That(weatherForecasts?.Count(), Is.EqualTo(3));
+        var (response, content) = await Api.Get<List<WeatherForecast>>("/WeatherForecast");
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(content, Has.Count.EqualTo(3));
+        });
     }
 }
